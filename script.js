eb7699e9fceb306a054b0fb115b0cc305a5a2626
Aaron <aaronmd99@gmail.com>
2021-10-31 22:32:55 -0400

    Made hamburger menu

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 //for loop to create new color scheme
 
 const colorPicker = function(){
-    let x = document.getElementsByClassName('newcolor')
+    let x = document.getElementsByClassName('newcolor');
     let hex = (Math.random() * 0xfffff * 1000000).toString(16);
     hex = '#' + hex.slice(0, 6);
-    for ( i =0; i<= x.length; i++){
+    for (let i =0; i<= x.length; i++){
         x[i].style.backgroundColor = hex;
     }
 }
+
+// changing 3 bar hamburger to x
+const burger = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav_menu');
+function mobilemenu() {
+    burger.classList.toggle("active");
+    nav.classList.toggle("active");
+    
+};
+
+burger.addEventListener("click",mobilemenu)
+
+
+
+
+
+    
+
